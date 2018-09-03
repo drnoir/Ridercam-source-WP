4407fd9aa53466c31269b1077a8aea8c3b465feb
Chris Godber <chrispgodber@gmail.com>
2018-09-03 15:17:17 +0100

    Messing with colour / style changes
    Switching to HTML5 Blank template after GULP not installing... Took out comments template on page.php to remove generic comments coming up

--- page.php
+++ page.php
@@ -28,10 +28,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 			<main class="site-main" id="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-
 					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-
 				<?php endwhile; // end of the loop. ?>
 
 			</main><!-- #main -->
